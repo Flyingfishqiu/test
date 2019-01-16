d955b64e13653efa6dc9bb936309fa0d9d462938
Your Name <you@example.com>
2019-01-16 09:05:18 +0800

    jianshu add mysql_config

---
+++ jianshu/commom/db/handle_mysql.py
@@ -0,0 +1,95 @@
+import logging
+import time
+from commom.db.read_mysql import Read_mysql
+import pymysql
+
+
+class DB(object):
+    def __init__(self, config_file):
+        self.read_mysql = Read_mysql(config_file)
+        try:
+            # 创建连接
+            self.conn = pymysql.Connect(host=self.read_mysql.host, port=int(self.read_mysql.port), user=self.read_mysql.user, password=self.read_mysql.password, db=self.read_mysql.dbname, charset='utf8')
+        except pymysql.err.OperationalError:
+            logging.error('数据库密码错误')
+
+    def select_data(self, sql):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql)
+
+            self.close_conn()
+        except pymysql.err.MySQLError:
+            logging.error('sql 语句执行错误')
+        else:
+            return cur.fetchall()
+
+    # 添加数据
+
+    def insert_data(self, table_name, table_data):
+        for key in table_data:
+            table_data[key] = "'" + str(table_data[key]) + "'"
+
+        key = ','.join(table_data.keys())
+        values = ','.join(table_data.values())
+        try:
+            sql = "INSERT INTO " + table_name + " (" + key + ") " + "VALUES" + " (" + values + ") "
+            with self.conn.cursor() as cur:
+                cur.execute(sql)
+            self.conn.commit()
+            self.close_conn()
+
+        except pymysql.err.ProgrammingError:
+            logging.error("sql语句 语法 错误")
+            self.conn.rollback()
+        except pymysql.err.IntegrityError as e:
+            logging.error("ID重复  msg : %s" % e)
+        except pymysql.err.InternalError as e:
+            logging.error("列名错误 msg : %s" % e)
+        except Exception as e:
+            logging.error(e)
+
+    # 清除表中所有数据
+    def clear_all(self, table_name):
+        try:
+            sql = "delete from " + table_name + ";"
+            with self.conn.cursor() as cur:
+                cur.execute("SET FOREIGN_KEY_CHECKS=0;")
+                cur.execute(sql)
+            self.conn.commit()
+            self.close_conn()
+        except pymysql.err.MySQLError:
+            self.conn.rollback()
+        except Exception as e:
+            self.conn.rollback()
+            logging.error(e)
+
+    def clears(self, sql):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql)
+            self.conn.commit()
+            self.close_conn()
+        except Exception as e:
+            logging.error("删除指定数据错误 msg : " % e)
+            self.conn.rollback()
+
+    # 关闭连接
+    def close_conn(self):
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    # info_category
+    table_data = {
+        "create_time": "2019-01-19 14:22:14",
+        "update_time": time.strftime("%Y-%m-%y %H:%M:%S", time.localtime()),
+        "id": 13,
+        "name": "test"
+    }
+    file = "./db_config.ini"
+    db = DB(file)
+    db.insert_data("info_category", table_data)
+    # print(db.select_data("select * from info_category;"))
+
+
--- jianshu/commom/operation_excle.py
+++ jianshu/commom/operation_excle.py
@@ -3,8 +3,7 @@ from xlutils3 import copy
 
 
 # f = xlrd.open_workbook('')
-# tabel =f.get_sheet()[0]
-# tabel.cell_value()
+# tabel =f.sheets()[0]
 
 
 class OperationExcle(object):
@@ -13,7 +12,7 @@ class OperationExcle(object):
             self.path = path
             self.index = index
         else:
-            self.path = 'D:\\测试\\project\\jianshu\\data\\test.xlsx'
+            self.path = '../data/test.xls'
             self.index = 0
         self.table = self.get_table()
 
@@ -35,8 +34,35 @@ class OperationExcle(object):
         table.write(row, col, value)
         copy_data.save(self.path)
 
+    # 根据case_id 获取对应行的内容
+    def get_case_id_to_value(self, case_id):
+        row = self.get_case_id_row(case_id)
+        return self.get_row_value(row)
+
+    # 根据 行号获取对应的内容
+    def get_row_value(self, row):
+        return self.table.row_values(row)
+
+    # 根据 case_id 获取行号
+    def get_case_id_row(self, case_id):
+        cols = self.get_cols_data()
+        nomber = 0
+        for col in cols:
+            if case_id in col:
+                return nomber
+            nomber += 1
+        return nomber
+
+    # 根据 列号 获取对应列的数据
+    def get_cols_data(self, col=None):
+        if col:
+            cols = self.table.col_values(col)
+        else:
+            cols = self.table.col_values(0)
+        return cols
+
 
 if __name__ == '__main__':
-    path = 'D:\\测试\\project\\jianshu\\data\\test2.xlsx'
-    file = OperationExcle(path)
-    file.write_sheet(3, 8, 'pass')
+    # path = 'D:\\测试\\project\\jianshu\\data\\test2.xlsx'
+    file = OperationExcle()
+    # file.write_sheet(3, 8, 'pass')
